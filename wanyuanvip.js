265575f7f8632887fb189ad556bada19abd98d9f
kangkang623 <81904221+kangkang623@users.noreply.github.com>
2024-11-03 16:51:54 +0800

    Update wanyuanvip.js

--- wanyuanvip.js
+++ wanyuanvip.js
@@ -12,8 +12,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/wanyuanyuedu.js
-
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/kangkang623/ziyongjs/refs/heads/main/wanyuanvip.js
 [mitm] 
 
 hostname = buy.itunes.apple.com
